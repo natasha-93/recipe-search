1c99107f1e448382aef66721d56d8d4fb4e88719
Natasha Lawson <natasha.lawson93@gmail.com>
2020-04-29 22:19:37 -0600

    Updates

---
+++ precache-manifest.bfb659062be95fbc761e9be510b6b07f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22e35d733d3eb2f2d0be2bb0ffc0cb65",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "ae6fc62255164267a65c",
+    "url": "/recipe-app/static/css/main.30630ddf.chunk.css"
+  },
+  {
+    "revision": "eadcedac76f94e3386d5",
+    "url": "/recipe-app/static/js/2.e1fb878a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-app/static/js/2.e1fb878a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae6fc62255164267a65c",
+    "url": "/recipe-app/static/js/main.84c896c3.chunk.js"
+  },
+  {
+    "revision": "6c108a12729393597590",
+    "url": "/recipe-app/static/js/runtime-main.2b464d9d.js"
+  }
+]);
